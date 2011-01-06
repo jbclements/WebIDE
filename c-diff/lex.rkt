db52beb4d38ed6a4c4cb6743acec868470b29876
John Clements <clements@racket-lang.org>
2011-01-06 12:10:09 -0800

    retabbing

--- c-diff/lex.rkt
+++ c-diff/lex.rkt
@@ -3,11 +3,14 @@
 ;; initial code by Bill Hess, 2011-01-03
 
 (require parser-tools/lex
-         test-engine/racket-tests
          (planet dherman/c:3:2/ast)
          (planet dherman/c:3:2/private/lexer))
 
-(provide c-lex-diff missing-code missing-code? missing-code-start missing-code-end)
+(provide c-lex-diff 
+         missing-code
+         missing-code?
+         missing-code-start
+         missing-code-end)
 
 (struct missing-code (start end))
 
@@ -46,16 +49,23 @@
                 [next-from (end-offset (car two))])
         (cond
           [(equal? t1 t2) (diff-tokens (cdr one) (cdr two) next-from end)]
-          [(and (not (empty? (cdr two))) (equal? t1 (position-token-token (second two))))
-           (cons (src-from-token (car two)) (diff-tokens one (cdr two) next-from end))]
-          [(and (not (empty? (cdr one))) (equal? t2 (position-token-token (second one))))
-           (cons (missing-code from (start-offset (car two))) (diff-tokens (cdr one) two from end))]
-          [else (cons (src-from-token (car two)) (diff-tokens (cdr one) (cdr two) next-from end))]))]))
+          [(and (not (empty? (cdr two))) 
+                (equal? t1 (position-token-token (second two))))
+           (cons (src-from-token (car two))
+                 (diff-tokens one (cdr two) next-from end))]
+          [(and (not (empty? (cdr one)))
+                (equal? t2 (position-token-token (second one))))
+           (cons (missing-code from (start-offset (car two)))
+                 (diff-tokens (cdr one) two from end))]
+          [else (cons (src-from-token (car two))
+                      (diff-tokens (cdr one) (cdr two) next-from end))]))]))
 
 ;(check-expect (diff-tokens (c-token-list "3 + 5") (c-token-list "3 + 5")) empty)
 ;(diff-tokens (c-token-list "3 + + 5") (c-token-list "3 + 5"))
 
 (define (c-lex-diff one two)
-  (diff-tokens (c-token-list one) (c-token-list two) 1 (+ 1 (string-length two))))
+  (diff-tokens (c-token-list one)
+               (c-token-list two)
+               1
+               (+ 1 (string-length two))))
 
-;(test)
