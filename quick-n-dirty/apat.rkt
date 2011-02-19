8e397f63bdab25d05c575cb2ae04e417fec528f3
John Clements <clements@racket-lang.org>
2011-02-18 17:07:59 -0800

    added awesome new apat abstraction\!

---
+++ quick-n-dirty/apat.rkt
@@ -0,0 +1,96 @@
+#lang racket
+
+(require (for-syntax syntax/parse)
+         rackunit)
+
+(provide apat
+         standardize/shallow)
+
+(define-syntax (apat stx)
+  (syntax-parse stx
+    [(_ (tag:id (attrname:id ... . other-attrsid:id) . elementsid:id) 
+        body:expr)
+     #'(cons (quote tag) 
+             (standardize/shallow
+              (lambda (tagid attrs . elementsid)
+                (match attrs
+                  [(cons '@
+                         (list-no-order (list (quote attrname) attrname) ... 
+                                        other-attrsid (... ...)))
+                   body]
+                  [other (error 'apat "missing required attribute from ~a" 
+                                (quote (attrname ...)))]))))]))
+
+;; take a function *requiring* sxml attributes
+;; and make it work for sxml without attributes
+(define (standardize/shallow fun)
+  (lambda (tag . others)
+    (match others
+      [`((@ ,attrs ...) ,elements ...)
+       (apply fun tag (cons '@ attrs) elements)]
+      [elements
+       (apply fun tag (cons '@ '()) elements)])))
+
+(check-equal? (apply
+               (standardize/shallow (lambda (a b . rest)
+                                      (list a b rest)))
+               `(tag (@ (attr1 "zip") (attr2 "zap")) "a" "b"))
+              (list 'tag
+                    '(@ (attr1 "zip") (attr2 "zap"))
+                    '("a" "b")))
+(check-equal? (apply
+               (standardize/shallow (lambda (a b . rest)
+                                      (list a b rest)))
+               `(tag  "a" "b"))
+              (list 'tag
+                    '(@ )
+                    '("a" "b")))
+
+;; regular match:
+(check-equal?
+ (apply 
+  (cdr 
+   [apat
+    (w1:segment (width height .  others) . content)
+    13])
+  `(w1:segment (@ (width "32") (height "49")) "a" "b"))
+ 13)
+;; missing required attribute:
+(check-exn
+ (lambda (exn)
+   (regexp-match #px"missing required attribute" (exn-message exn)))
+ (lambda ()
+   (apply 
+    (cdr 
+     [apat
+      (w1:segment (width height .  others) . content)
+      13])
+    `(w1:segment (@ (width "32")) "a" "b"))))
+;; extras should be okay:
+(check-equal?
+ (apply 
+  (cdr 
+   [apat
+    (w1:segment (width height .  others) . content)
+    others])
+  `(w1:segment (@ (height "49") (boogle "rap") (width "32")) "a" "b"))
+ `((boogle "rap")))
+;; we can actually access the attributes:
+(check-equal?
+ (apply 
+  (cdr 
+   [apat
+    (w1:segment (width height .  others) . content)
+    (list (+ (string->number width) (string->number height)) content)])
+  `(w1:segment (@ (height "49") (boogle "rap") (width "32")) "a" "b"))
+ (list 81 (list "a" "b")))
+;; one set of attrs
+(check-equal?
+ (apply 
+  (cdr 
+   [apat
+    (w1:segment attrs . content)
+    (list attrs content)])
+  `(w1:segment (@ (height "49") (boogle "rap") (width "32")) "a" "b"))
+ (list '((height "49") (boogle "rap") (width "32")) (list "a" "b")))
+
--- quick-n-dirty/xml-to-html.rkt
+++ quick-n-dirty/xml-to-html.rkt
@@ -1,13 +1,12 @@
 #lang racket
 
 (require (planet clements/sxml2)
-         (except-in (planet clements/sxml2/ssax/ssax) run-test foldts)
-         rackunit)
+         (except-in rackunit foldts))
 
-(provide path->xml
+
+#;(provide path->xml
          xml->steps
-         step->html
-         )
+         step->html)
 
 (define (xml->steps lab-xml)
   ((sxpath '(w1:lab w1:step)) lab-xml))
@@ -17,15 +16,25 @@
     (lambda (port)
       (ssax:xml->sxml port `((w1 . "http://www.web-ide.org/namespaces/labs/1"))))))
 
-;; given a step, produce the corresponding html content
-(define (step->html step)
-  (match step
-    [`(w1:step (@ ,attrs ...) ,content ...)
-     (unless (assoc 'name attrs)
-       (error 'step->html "step missing name attribute"))
-     (define name (second (assoc 'name attrs)))
-     `(div (h3 "step name: " ,name)
-           ,@(map process-content content))]))
+
+(define rules
+  (list
+   [apat
+    (w1:step (name . others) . content)
+    `(div (h3 "step name: " ,name) . content)]
+   [apat
+    (w1:evaluator any . content)
+    ""]
+   [apat
+    (w1:segment (width height others) . content)
+    `(textarea  (@ (width ,width)
+                   (height ,height))
+                "" ,@content)]
+   `(*default*
+     .
+     (lambda args args))))
+
+
 
 ;; mush the xml into plausible html:
 (define (process-content content)
@@ -40,22 +49,60 @@
                       (height ,(first (dict-ref attrs 'height))))
                    "" ,@strs)]
        ;; ignore the bogus ones that just hold evaluators:
-       [`((@ ,attrs ...) ,(? evaluator? e) ...) ""]
+       [`((@ ,attrs ...) ,(? (tag-checker 'w1:evaluator) e) ...) ""]
        [other (error 'process-content
                      "strange segment: ~a" content)])]
+    ;; rewrite grody GWT tables into HTML ones:
+    [`(w1:labtable (@ ,attrs ...) ,adds ...)
+     (process-table attrs adds)]    
     [`(,tag (@ ,attrs ...) ,content ...)
      `(,(strip-tag tag) (@ ,@attrs) ,@(map process-content content))]
     [`(,tag ,content ...)
      `(,(strip-tag tag) ,@(map process-content content))]    
     [other other]))
 
+;; process-table : attr-list element-list -> sxml
+(define (process-table attrs adds)
+  (define height (num-attr attrs 'rows))
+  (define width (num-attr attrs 'cols))
+  (unless (andmap (tag-checker 'w1:add) adds)
+    (error 'process-content "expected only adds in table, got: ~a" adds))
+  (unless (integer? (/ (length adds) width))
+    (error 'process-content "number of table entries (~a) is not divisible by number of table columns (~a)" (length adds) width))
+  (unless (integer? (/ (length adds) height))
+    (error 'process-content "number of table entries (~a) is not divisible by number of table rows (~a)" (length adds) height))
+  (define grouped (let loop ([left adds])
+                    (cond [(empty? left) empty]
+                          [else (cons (process-table-row (take left width))
+                                      (loop (drop left width)))])))
+  `(table (@ (rows ,(number->string height))
+             (cols ,(number->string width)))
+          ,@grouped))
 
-(check-equal? (process-content `(w1:segment (@ (width "20") (id "fresh-id-1") (height "1"))))
-              `(textarea (@ (width "20") (height "1")) ""))
 
-;; is this element an evaluator?
-(define (evaluator? sxml)
-  (and (list? sxml) (equal? (first sxml) 'w1:evaluator)))
+;; process-table-row : elements -> sxml
+(define (process-table-row elements)
+  (define cells (map process-table-cell elements))
+  `(tr ,@cells))
+
+(define (process-table-cell element)
+  (match element
+    [`(w1:add (@ ,attrs ...) ,elts ...)
+     `(td (@ ,attrs ...) ,@(map process-content elts))]
+    [`(w1:add ,elts ...)
+     `(td ,@(map process-content elts))]))
+
+;; extract an attribute representing a number
+(define (num-attr attrs key)
+  (match (assoc key attrs)
+    [`(,dc ,num-str) (string->number num-str)]
+    [other (error 'num-attr "no ~a attribute found among ~a" key attrs)]))
+
+
+
+;; tag-checker : tag -> element -> boolean?
+(define ((tag-checker tag) element)
+  (and (list? element) (equal? (first element) tag)))
 
 ;; strip the colon-separated part of a symbol off.
 (define (strip-tag s)
@@ -63,10 +110,16 @@
     [(list match rhs) (string->symbol rhs)]
     [false (error 'strip-tag "tag without prefix: ~a" s)]))
 
+
+(check-equal? (num-attr '((a "13") (b "14")) 'a) 13)
+(check-equal? (num-attr '((a "13") (b "14")) 'b) 14)
+
+
+
 (check-equal? (strip-tag 'w1:br) 'br)
 
-(check-equal? (evaluator? `(3 w1:evaluator)) #f)
-(check-equal? (evaluator? `(w1:evaluator (@ (abc "def")))) #t)
+(check-equal? ((tag-checker 'w1:evaluator) `(3 w1:evaluator)) #f)
+(check-equal? ((tag-checker 'w1:evaluator) `(w1:evaluator (@ (abc "def")))) #t)
 
 (check-equal? (process-content `(w1:b (w1:i "zoobah")))
               `(b (i "zoobah")))
@@ -74,3 +127,28 @@
 (check-equal? (process-content `(w1:b (@ (awesomeness "35")) "trip"))
               `(b (@ (awesomeness "35")) "trip"))
 
+(check-equal? (process-table-cell `(w1:add "bc"))
+              `(td "bc"))
+
+(check-equal? (process-table-row `((w1:add "bc") (w1:add "de")))
+              `(tr (td "bc") (td "de")))
+
+(check-equal? (process-table `((rows "3") (cols "2"))
+                             `((w1:add "a1")
+                               (w1:add "a2")
+                               (w1:add "a3")
+                               (w1:add "a4")
+                               (w1:add "a5")
+                               (w1:add "a6")))
+              `(table (@ (rows "3") (cols "2"))
+                      (tr (td "a1") (td "a2"))
+                      (tr (td "a3") (td "a4"))
+                      (tr (td "a5") (td "a6"))))
+
+
+
+
+(check-equal? (process-content `(w1:segment (@ (width "20") (id "fresh-id-1") (height "1"))))
+              `(textarea (@ (width "20") (height "1")) ""))
+
+|#
