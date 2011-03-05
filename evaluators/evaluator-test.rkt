2cedefa33e8b55b378e53e332a06f440b2e624ed
John Clements <clements@racket-lang.org>
2011-03-05 05:00:41 -0800

    got evaluator protocol working

--- evaluators/evaluator-test.rkt
+++ evaluators/evaluator-test.rkt
@@ -2,35 +2,50 @@
 
 (require (planet dherman/json:3:0)
          net/url
-         net/uri-codec)
+         net/uri-codec
+         rackunit)
 
-
-(define sample-json
-  (string->bytes/utf-8
-  (string-append
-   "request="
-    (form-urlencoded-encode
-   (jsexpr->json 
-    #hasheq((id . "Facebook: Selection")
-            (args . #hasheq((functionCall . "assignGroup(18)")
-                            (fmessage . "I'm sorry, your if statement for group C is incorrect.")
-                            (expectedOutput . "'C'")
-                            (smessage . "Good Job!")
-                            (function . "public char assignGroup(int age) { char group = 'x'; @groupC return group;} ")))
-            (textfields . #hasheq((groupC . "abba zabba")))))))))
-
-(define (path->path/params los)
-  (map (lambda (s) (path/param s '())) los))
-
-(define the-url
-  (url "http" #f "184.73.238.21" #f #t 
-       (path->path/params '("webide""evaluators""JavaEvaluator" "JavaEvaluator.php")) 
-       `()
-       #f))
-
-(url->string the-url)
-(define port (post-impure-port the-url sample-json
+;; given a url and a string and an assoc-list of args and an assoc-list
+;; of textfields, return the response of the evaluator as a jsexpr
+(define (remote-evaluator-call url id args textfields)
+  (define post-bytes (jsexpr->post-bytes (make-eval-jsexpr id args textfields)))
+  ;; probably can't use pure-port for bad evaluators...
+  (define port (post-pure-port the-url sample-json
                                '("Content-Type: application/x-www-form-urlencoded")))
-(regexp-match #px".*" port)
-(close-input-port port)
+  ;; this probably isn't robust wrt bad evaluators...
+  (define reply (first (regexp-match #px".*" port)))
+  (close-input-port port)
+  (json->jsexpr (bytes->string/utf-8 reply)))
 
+(define (make-eval-jsexpr id args textfields)
+  (jsexpr->json 
+        (make-hasheq `((id . ,id)
+                       (args . ,(make-hasheq args))
+                       (textfields . ,(make-hasheq textfields))))))
+
+(define (jsexpr->post-bytes jsexpr)
+  (string->bytes/utf-8 (string-append "request=" (form-urlencoded-encode jsexpr))))
+
+;; an example:
+
+(define sample-args 
+  '((functionCall . "assignGroup(18)")
+    (fmessage . "I'm sorry, your if statement for group C is incorrect.")
+    (expectedOutput . "'C'")
+    (smessage . "Good Job!")
+    (function . "public char assignGroup(int age) { char group = 'x'; @groupC return group;} ")))
+
+(define (success-response msg)
+  (hash-set #hasheq((status . "success")) 'message msg))
+#;(define (fail-response msg)
+  (hash-set #hasheq((status . "failure")) 'message msg))
+
+(define amazon-evaluator
+  "http://184.73.238.21/webide/evaluators/JavaEvaluator/JavaEvaluator.php")
+
+(check-equal?
+ (remote-evaluator-call (string->url amazon-evaluator)
+                        "Facebook: Selection"
+                        sample-args
+                        '((groupC . "group = 'C';")))
+ (success-response "Good Job!"))
