a319f7f13735c899b449e22669a53b24e14c2ee1
John Clements <clements@racket-lang.org>
2011-03-29 11:47:21 -0700

    log reader improvement

--- evaluators/mongodb-log-reader.rkt
+++ evaluators/mongodb-log-reader.rkt
@@ -2,6 +2,7 @@
 
 (require (planet jaymccarthy/mongodb)
          (planet dherman/json)
+         srfi/19
          web-server/http/request-structs
          net/url)
 
@@ -18,11 +19,35 @@
     (match bindings
       [(vector (hash-table ('id #"request")
                            ('value bytes)))
-       (list (seconds->date (hash-ref r 'timestamp))
-             (hash-ref request 'uri)
-             (json->jsexpr (bytes->string/utf-8 bytes)))]
+       (define jsexpr (json->jsexpr (bytes->string/utf-8 bytes)))
+       (match jsexpr
+         [(hash-table ('id id)
+                      ('args args)
+                      ('textfields textfields))
+          (list (hash-ref r 'timestamp)
+                (list (hash-ref request 'uri)
+                      args
+                      id)
+                textfields)]
+         [other
+          (list (hash-ref r 'timestamp)
+                (list 'bogus-problem
+                      other)
+                (hash))])]
       [other (error 'extract-all-requests
                     "unexpected format for bindings: ~s" 
                     other)])))
 
-(extract-all-submissions)
+(define (group-by-problem submissions)
+  (define ht
+    (for/fold ([ht (hash)])
+      ([p (in-list submissions)])
+      
+      (hash-set ht (second p) (cons (list (date->string
+                                           (time-utc->date (first p))
+                                           "~4")
+                                          (hash-map (third p) list))
+                                    (hash-ref ht (second p) empty)))))
+  (hash-map ht list))
+
+(group-by-problem (extract-all-submissions))
