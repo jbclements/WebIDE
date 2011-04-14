e8b8fb6fb6c3e0bc1f051f74e343428d3e0cae19
John Clements <clements@racket-lang.org>
2011-04-13 17:49:45 -0700

    combined log-reader with log-parser

--- evaluators/mongodb-log-reader.rkt
+++ evaluators/mongodb-log-reader.rkt
@@ -11,6 +11,8 @@
 (define requests (mongo-collection d "requests"))
 (define results (mongo-collection d "results"))
 
+(printf "number of results: ~s\n" (mongo-collection-count results))
+
 #;(define (extract-failing-submissions)
   (for/list ([r (mongo-collection-find results '())])
     (define request (hash-ref r 'request))
@@ -60,9 +62,9 @@
                 (list 'bogus-problem
                       other)
                 (hash))])]
-      [other (error 'extract-all-requests
-                    "unexpected format for bindings: ~s" 
-                    other)])))
+      [other (list (hash-ref r 'timestamp)
+                   (list 'empty-bindings)
+                   (hash))])))
 
 (define (group-by-problem submissions)
   (define ht
@@ -76,4 +78,29 @@
                                     (hash-ref ht (second p) empty)))))
   (hash-map ht list))
 
-(group-by-problem (extract-all-submissions))
+(define probs (group-by-problem (extract-all-submissions)))
+
+
+
+
+(define (process-problem p)
+  (match p
+    [(list (list uri args "First C Lab") submissions)
+     (list (list uri args "First C Lab")
+           (collect-with-stats submissions))]
+    [other
+     (fprintf (current-error-port)
+              "ignoring problem: ~s\n" (first p))]))
+
+(define (collect-with-stats submissions)
+  (define ht
+    (for/fold ([ht (hash)])
+      ([s (in-list submissions)])
+      (match s 
+        [(list timestamp (list (list dc text)))
+         (hash-set ht text (+ 1 (hash-ref ht text 0)))]
+        [other 
+         (error 'collect-with-stats "unexpected texts: ~s" other)])))
+  (sort (hash-map ht list) > #:key second))
+
+
