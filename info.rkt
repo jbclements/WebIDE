e3247096dcb7a3979db287a4712764cc4195739b
John Clements <clements@racket-lang.org>
2016-03-30 17:09:43 -0700

    add deps

--- info.rkt
+++ info.rkt
@@ -2,4 +2,14 @@
 
 (define collection 'multi)
 
-(define deps (list "java" "c-utils" "sxml" "mongodb"))
+(define deps (list
+              "base"
+              "math-lib"
+              "parser-tools-lib"
+              "rackunit-lib"
+              "srfi-lite-lib"
+              "web-server-lib"
+              "java" "c-utils" "sxml" "mongodb"))
+
+(define build-deps
+  (list "at-exp-lib"))
